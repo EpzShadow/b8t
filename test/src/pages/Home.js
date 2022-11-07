35e9051e38309e0207e62b1082111a91e7059816
annyzwei <86426844+annyzwei@users.noreply.github.com>
2022-11-06 21:20:14 -0500

    courtesy of cheran

---
+++ test/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = () => {
+
+
+    return (
+      <div className="home">
+        <p> please work </p>
+        <form action="http://localhost:5000/upload" enctype="multipart/form-data" method="POST">
+          <input type="file" name="pic" />
+          <input type="submit" value="Upload a file"/>
+        </form>
+      </div>
+    );
+  }
+
+export default Home;
