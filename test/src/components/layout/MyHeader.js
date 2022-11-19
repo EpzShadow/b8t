f4f5e7a46b68c579b711ea31aa649e495fe6b11b
EpzShadow <85049418+EpzShadow@users.noreply.github.com>
2022-11-19 18:57:29 -0500

    changed the entirety of src and index.js

---
+++ test/src/components/layout/MyHeader.js
@@ -0,0 +1,15 @@
+import logo from './stolen.png';
+import classes from './cssfiles/MyHeader.module.css';
+import SearchBar from './SearchBar';
+function MyHeader(){
+    // console.log("HERE");
+    return (
+        <div className = {classes.container} >
+            <img src = {logo} alt = "logo" className = {classes.image} />
+            <div className = {classes.searchBar}>
+                <SearchBar />
+            </div>
+        </div>
+    )
+}
+export default MyHeader;
